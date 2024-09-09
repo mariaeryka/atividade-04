396e381ef703105adaee0100d0cfbdbe85595ef8
mariaeryka <erykamariab4@gmail.com>
2024-09-09 10:11:20 -0300

    altercao

---
+++ att 04.py
@@ -0,0 +1,10 @@
+# Crie um programa que receba três notas de um aluno e calcule a média aritmética.
+
+N1= int(input("sua 1 nota"))
+N2= int(input("sua 2 nota"))
+N3= int(input("sua 3 nota"))
+
+med = (N1 + N2 + N3)/3
+
+print (F"sua media é {med}")
+
